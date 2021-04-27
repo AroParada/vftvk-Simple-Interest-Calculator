ff742393ca4f7a132780ed1475ede0a9ff314a8e
harold parada <haroldparada20@gmail.com>
2021-04-26 21:54:38 -0400

    fixed javascript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function compute()
 {
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
+    // r = 12
     y = document.getElementById("years").value;
  
     var result = parseInt(p)*(parseFloat(r)/100)*parseInt(y);
