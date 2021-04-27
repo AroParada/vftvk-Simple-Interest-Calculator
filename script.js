bafe8f517d34dee921d0aae54e0f252d0aeb135a
harold parada <haroldparada20@gmail.com>
2021-04-26 21:49:52 -0400

    added some javascript

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+ 
+    var result = parseInt(p)*(parseFloat(r)/100)*parseInt(y);
+    alert(result)
 }
         
